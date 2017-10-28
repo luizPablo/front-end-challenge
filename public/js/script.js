85208f89a7d2953d02f8ea6a4c172b2ee5dec37d
Luiz Pablo <lpablotavares@gmail.com>
2017-10-28 00:27:46 -0300

    organização de pastas e códigos | navbar concluída

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+  //codigo jquery utilizado
+  $(document).ready(function($) {
+    //utilizando objeto jquery para tratar o iframe (deixar mudo)
+    $("#video").vimeo("setVolume", 0);
+
+    //codigo utilizado no menu de telefones 
+    $('.tel-item').on('click', function(){
+      $(this).parent().find('ul').toggle('slow');
+      $(this).parent().find('ul li span.nome').css('font-weight', 300);
+      if($($(this).children()[1]).hasClass('fa-angle-down')){
+        $($(this).children()[1]).removeClass('fa-angle-down');
+        $($(this).children()[1]).addClass('fa-angle-up');
+      }else{
+        $($(this).children()[1]).addClass('fa-angle-down');
+        $($(this).children()[1]).removeClass('fa-angle-up');
+      }
+    });
+  });
