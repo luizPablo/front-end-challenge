44348c49882af4f40cbe08f8e2ae2e90c55fbf34
Luiz Pablo <lpablotavares@gmail.com>
2017-10-31 00:04:40 -0300

    showcase 100%

--- public/js/script.js
+++ public/js/script.js
@@ -16,11 +16,31 @@
       }
     });
 
+    //colocar uma cor na navbar, ao rolar pra baixo
     $(window).on("scroll", function() {
       if($(window).scrollTop() > 100) {
         $(".showcase-nav").addClass("backblack");
       } else {
         $(".showcase-nav").removeClass("backblack");
       }
-  });
+    });
+
+    $('label').on('click', function(){
+      $('label').each(function(index){
+        $(this).css('background-color', '#1c1c1c');
+        $(this).css('border', '2px solid #fff');
+      });
+      $(this).css('background-color', '#fff');
+    })
+
+    $('.check_pgp').on('click', function(){
+      if($(this).hasClass('fa-square-o')){
+        $(this).removeClass('fa-square-o');
+        $(this).addClass('fa-square');
+        //o campo checkbox não existe no HTML, mas caso existisse poderia dar o check nele aqui pelo js
+      }else{
+        $(this).addClass('fa-square-o');
+        $(this).removeClass('fa-square');
+      }
+    })
   });
